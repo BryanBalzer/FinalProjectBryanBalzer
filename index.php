491135b206602a77f0726f05344fad94e14dc113
BryanBalzer <94866324+BryanBalzer@users.noreply.github.com>
2022-11-28 14:39:21 -0600

    Added text to header.php, footer.php and index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require 'connect.php';
+?>
+
+
+
+<?php include 'header.php'; ?>
+
+<div id="wrapper">
+    <div id="content">
+       
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
