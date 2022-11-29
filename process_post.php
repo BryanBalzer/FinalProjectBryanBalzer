98ea6c742017e48de83a133911254dc06823d90b
BryanBalzer <94866324+BryanBalzer@users.noreply.github.com>
2022-11-28 19:40:34 -0600

    Added code to css, process_post and create files

--- process_post.php
+++ process_post.php
@@ -63,4 +63,30 @@ if ($_POST['command'] == 'Delete User') {
     $statement->execute();
     header('Location: admin.php');
 }
+
+if ($_POST['command'] == 'Create') {
+    if (!empty($_POST['post_title']) && (!empty($_POST['post_review'])) && (!empty($_POST['movies']))) {
+        $post_title = filter_input(INPUT_POST, 'post_title', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $post_review = filter_input(INPUT_POST, 'post_review', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $movies =  filter_input(INPUT_POST, 'movies', FILTER_SANITIZE_NUMBER_INT);
+
+        $query = "SELECT userid FROM users WHERE username = :username";
+        $values = $db->prepare($query);
+        $values->bindValue(':username', $_SESSION['username']);
+        $values->execute();
+        $row = $values->fetch();
+
+        } else {
+            $query = "INSERT INTO posts (userid, post_title, post_review, movie_id) values (:userid, :post_title, :post_review, :movie_id)";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':userid', $row['userid']);
+            $statement->bindValue(':post_title', $post_title);
+            $statement->bindValue(':post_review', $post_review);
+            $statement->bindValue(':movie_id', $movies);
+            $statement->execute();
+        }
+        header('Location: index.php');
+    } else {
+        $error = true;
+    }
 ?>
